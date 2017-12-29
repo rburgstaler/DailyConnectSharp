d07df85742304f80327c140227479a4f82c116f4
Rick Burgstaler <rburgstaler@yahoo.com>
2017-12-29 15:11:20 -0600

    Get a quick implementation working where we can get the summary of the activity for the day

--- DailyConnectSharp/Form1.Designer.cs
+++ DailyConnectSharp/Form1.Designer.cs
@@ -28,12 +28,97 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btGo = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tbUserName = new System.Windows.Forms.TextBox();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btGo
+            // 
+            this.btGo.Location = new System.Drawing.Point(34, 24);
+            this.btGo.Name = "btGo";
+            this.btGo.Size = new System.Drawing.Size(195, 64);
+            this.btGo.TabIndex = 0;
+            this.btGo.Text = "Go";
+            this.btGo.UseVisualStyleBackColor = true;
+            this.btGo.Click += new System.EventHandler(this.btGo_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(5, 126);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(659, 271);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.WordWrap = false;
+            // 
+            // tbUserName
+            // 
+            this.tbUserName.Location = new System.Drawing.Point(359, 24);
+            this.tbUserName.Name = "tbUserName";
+            this.tbUserName.Size = new System.Drawing.Size(252, 20);
+            this.tbUserName.TabIndex = 2;
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Location = new System.Drawing.Point(359, 50);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.Size = new System.Drawing.Size(252, 20);
+            this.tbPassword.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(282, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(285, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Password";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(666, 403);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.tbUserName);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btGo);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btGo;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox tbUserName;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- DailyConnectSharp/Form1.cs
+++ DailyConnectSharp/Form1.cs
@@ -1,9 +1,14 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +17,151 @@ namespace DailyConnectSharp
 {
     public partial class Form1 : Form
     {
+        public static String PrefPath()
+        {
+            string codeBase = Assembly.GetExecutingAssembly().CodeBase;
+            UriBuilder uri = new UriBuilder(codeBase);
+            String exePath = Uri.UnescapeDataString(uri.Path);
+            return Path.ChangeExtension(exePath, ".json");
+
+        }
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void msgCallback(String msg)
+        {
+            textBox1.AppendText(msg + Environment.NewLine);
+        }
+
+        public static string GetResponseContent(WebResponse resp)
+        {
+            if (resp == null) return "";
+            using (StreamReader reader = new StreamReader(resp.GetResponseStream())) return reader.ReadToEnd();
+        }
+
+        public void FormToPrefs(Prefs pref)
+        {
+            pref.Username = tbUserName.Text;
+            pref.Password = tbPassword.Text;
+        }
+        public void PrefsToForm(Prefs pref)
+        {
+            tbUserName.Text = pref.Username;
+            tbPassword.Text = pref.Password;
+        }
+
+        public String PrettyfyString(String content)
+        {
+            try
+            {
+                JObject jsObj = JsonConvert.DeserializeObject<JObject>(content);
+                return JsonConvert.SerializeObject(jsObj, Formatting.Indented);
+            }
+            catch
+            {
+                return content;
+            }
+        }
+
+        public bool PerformRequest(CookieContainer cookieContainer, String url, String content, out String respCont)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.CookieContainer = cookieContainer;
+
+            request.Method = "POST";
+
+
+            // set request body
+            request.ContentType = "application/x-www-form-urlencoded";
+            using (StreamWriter writer = new StreamWriter(request.GetRequestStream())) writer.Write(content);
+
+            // GetResponse raises an exception on http status code 400
+            // We can pull response out of the exception and continue on our way            
+            try
+            {
+                HttpWebResponse resp = request.GetResponse() as HttpWebResponse;
+                respCont = GetResponseContent(resp);
+                msgCallback($"Content {resp.StatusCode} - {resp.StatusDescription}");
+                msgCallback(PrettyfyString(respCont));
+                msgCallback("Cookie");
+
+                foreach (Cookie cook in cookieContainer.GetCookies(new Uri(url)))
+                {
+                    msgCallback($"{cook.Name}={cook.Value}");
+                }
+            }
+            catch (WebException ex)
+            {
+                throw new Exception(String.Format("Error processing request: {0}, Response: {1}", ex.Message, GetResponseContent(ex.Response)));
+            }
+            return true;
+        }
+
+        private void btGo_Click(object sender, EventArgs e)
+        {
+            Prefs cp = new Prefs();
+            FormToPrefs(cp);
+            File.WriteAllText(PrefPath(), JsonConvert.SerializeObject(cp, Formatting.Indented));
+
+
+            String resStr = "";
+            CookieContainer cookieContainer = new CookieContainer();
+            PerformRequest(cookieContainer, "https://www.dailyconnect.com/Cmd?cmd=UserAuth", "email=" + cp.Username + "&pass=" + cp.Password, out resStr);
+            PerformRequest(cookieContainer, "https://www.dailyconnect.com/CmdW", "cmd=UserInfoW", out resStr);
+
+            UserInfoW userInfo = JsonConvert.DeserializeObject<UserInfoW>(resStr);
+
+
+            foreach (Child child in userInfo.myKids)
+            {
+                msgCallback($"{child.Name} - {child.Id}");
+                PerformRequest(cookieContainer, "https://www.dailyconnect.com/CmdW", "cmd=KidGetSummary&Kid="+child.Id+"&pdt=171223", out resStr);
+            }
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Prefs pref = (File.Exists(PrefPath())) ? JsonConvert.DeserializeObject<Prefs>(File.ReadAllText(PrefPath())) : new Prefs();
+            PrefsToForm(pref);
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Prefs cp = new Prefs();
+            FormToPrefs(cp);
+            File.WriteAllText(PrefPath(), JsonConvert.SerializeObject(cp, Formatting.Indented));
+        }
+    }
+
+    public class Child
+    {
+        public String Name { get; set; }
+        public String Id { get; set; }
+        public String Dummy { get; set; }
+    }
+
+    public class UserInfoW
+    {
+        public Child[] myKids { get; set; }
+        public UserInfoW()
+        {
+            myKids = new Child[0];
+        }
+    }
+
+    public class Prefs
+    {
+        public String Username { get; set; }
+        public String Password { get; set; }
+        public Prefs()
+        {
+            Username = "";
+            Password = "";
         }
     }
 }
